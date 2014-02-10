9d2a99dcbb51e3365cad6afc166f64c203c1d7ec
Johan Hassel <johan_sr7@hotmail.com>
2014-02-10 20:01:12 +0100

    Changed ADSampler to interface to XADC IP

--- Source/AudioIO/ADSampler.vhdl
+++ Source/AudioIO/ADSampler.vhdl
@@ -4,7 +4,6 @@ library ieee ;
 
 entity ADSampler is
 	port (
-
 		--http://www.xilinx.com/support/documentation/user_guides/ug480_7Series_XADC.pdf
 
 		DRP_output : in std_logic_vector(15 downto 0);
@@ -16,9 +15,14 @@ entity ADSampler is
 		DRP_writeEnable : out std_logic;
 		DRP_clk : out std_logic;
 
+		XADC_EOC : in std_logic;
+		XADC_busy : in std_logic;
 		XADC_reset : out std_logic;
-		XADC_convst : out std_logic;
-		XADC_convstclk : out std_logic;
+
+		-- Probably handled by the IP core
+		--DRP_clk : out std_logic;
+
+		output : out std_logic_vector(11 downto 0);
 
 		clk : in std_logic;
 		reset : in std_logic
@@ -26,52 +30,77 @@ entity ADSampler is
 end entity ; -- ADSampler
 
 architecture arch of ADSampler is
-	-- Address to the value of the first port A/D converter
-	constant address_input : std_logic_vector(6 downto 0) := x"10";
+	constant address_input : std_logic_vector(6 downto 0) := x"03";
 
-	-- Address to the flag register
-	constant address_flags : std_logic_vector(6 downto 0) := x"3F";
-
-	-- ADdress to config registers
-	constant address_config0 : std_logic_vector(6 downto 0) := x"40";
-	constant address_config1 : std_logic_vector(6 downto 0) := x"41";
-	constant address_config2 : std_logic_vector(6 downto 0) := x"42";
-
-	type state_type is (config_flags, config_r0, config_r1, config_r2, sample);
+	type state_type is (res, busy, busy_conversion, read);
 
 	type reg_type is record
 		state : state_type;
+
+		output : std_logic_vector(11 downto 0);
+
+		DRP_enable : std_logic;
+		-- Probably handled by the IP core
+		--DRP_clk : std_logic;
 	end record;
 
 	signal r, rin : reg_type;
 begin
-	-- Tie to global clock according to XADC spec.
-	XADC_convst <= clk;
+
+	DRP_clk <= clk;
+	XADC_reset <= reset;
+	DRP_address <= address_input;
+	DRP_enable <= r.DRP_enable;
+	DRP_writeEnable <= '0';
 	DRP_input <= (others => '0');
 
 	clk_proc : process( clk, reset )
 	begin
 		if(reset = '1') then
-			r.state <= config;
+			r.state <= res;
+			r.output <= (others => '0');
 		elsif(rising_edge(clk)) then
 			r <= rin;
 		end if;
 	end process ; -- clk_proc
 
-	comb_proc : process( r, rin, DRP_output, DRP_dataReady )
+	comb_proc : process( r, rin, DRP_output, DRP_dataReady, XADC_busy, XADC_EOC )
 		variable v : reg_type;
 	begin
 		v := r;
 
+		v.DRP_enable := '0';
+
 		case r.state is
-			when config_flags =>
-				-- Config flags register;
-			when config_r0 =>
-				-- Config register 0
-			when config_r1 =>
-				-- Config register 1;
-			when config_r2 =>
-				-- Config register 2;
+			when res =>
+				-- Reset the XADC (done strucutrally, just go to next state)
+				v.state := busy;
+
+			when busy =>
+				-- Wait for the XADC to become ready
+				-- (This state might not be necessary)
+				if(XADC_busy = '0') then
+					v.state := busy_conversion;
+				end if;
+
+			when busy_conversion =>
+				-- Wait for the DRP to acquire the data
+				if(XADC_EOC = '1') then
+					-- Data is available in the DRP, read it.
+					v.DRP_enable := '1';
+					v.state := read;
+				end if;
+
+			when read =>
+				-- Wait for the DRP data to become ready
+				if(DRP_dataReady = '1') then
+					v.state := busy_conversion;
+
+					-- Read the DRP output
+					v.output := DRP_output;
+
+				end if;
+
 			when others =>
 				-- Don't care
 		end case;
