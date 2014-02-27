c135532dde6f60d3cf9164ac709ecd7586ba7a5d
dovan89 <niklasdoverbo@hotmail.com>
2014-02-27 21:36:08 +0100

    Added comments and made small adjustments
    The code is now much clearer on what it does

--- Source/Generic Filters/Generic_IIR_SO.vhd
+++ Source/Generic Filters/Generic_IIR_SO.vhd
@@ -1,23 +1,47 @@
-
-------------------------------------------------------
-------------------------------------------------------
--- Description:                                     --
--- Implementation of a generic direct IIR-filter    --
--- Note that the lower bits will be binals          --
---                                                  --
--- Input/Output:                                    --
--- WIDTH      - Width of input and output           --
--- N          - Number of coefficients              --
--- clk        - Clock                               --
--- clk_en     - Clock enable, takes input when high --
--- x          - Input, WIDTH bits                   --
--- y          - Output, WIDTH bits                  --
---                                                  --
--- Internal Constants                               --
--- N_WIDTH    - Width of the coefficients           --
--- N_BINALS   - Number of binals in the coefficients--
-------------------------------------------------------
-------------------------------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Description:                                                               --
+-- Implementation of a discrete-time, second-order, direct-form I IIR filter. --
+-- This filter uses fixed point arithmetic to do it's calculations and can be --
+-- described by the following mathematical formula:                           --
+-- Y[k] = B0*X[k] + B1*X[k-1] + B2*X[k-2] + A1*Y[k-1] + A2*Y[k-1]             --
+--                                                                            --
+--                                                                            --
+-- Generic:                                                                   --
+-- IN_WIDTH          - The width of the input signal                          --
+-- IN_FRACT          - The width of the fractional part of the input signal   --
+-- COEFFICIENT_WIDTH - The width of the filter coefficients                   --
+-- COEFFICIENT_FRACT - The width of the fractional part of the filter         --
+--                     coefficients                                           --
+-- INTERNAL_WIDTH    - The width of internal states of the filter             --
+-- INTERNAL_FRACT    - The width of the fractional part of internal states in --
+--                     the filter                                             --
+-- OUT_WIDTH         - The width of the output signal                         --
+-- OUT_FRACT         - The width of the fractional part of the output signal  --
+--                                                                            --
+--                                                                            --
+-- Input/Output:                                                              --
+-- clk               - System clock                                           --
+-- clk_en            - Clock enable signal, enabled when high                 --
+-- reset             - Asynchronous reset that resets when high               --
+-- x                 - Input signal to be filtered                            --
+-- B0                - Coefficient                                            --
+-- B1                - Coefficient                                            --
+-- B2                - Coefficient                                            --
+-- A1                - Coefficient                                            --
+-- A2                - Coefficient                                            --
+-- y                 - Output signal                                          --
+--                                                                            --
+--                                                                            --
+-- Internal Constants                                                         --
+-- N                 - Number of coefficients, this number is three for a     --
+--                     second order filter and should not be changed. The     --
+--                     constant is mearly there to simplify creation of       --
+--                     higher order filters. Note that for this to be done    --
+--                     successfully, you have to increase the number of       --
+--                     coefficients as well.                                  --
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -28,15 +52,16 @@ use ieee.numeric_std.all;
 
 entity Generic_IIR_SO is
    generic (IN_WIDTH          : integer := 16;
-            IN_FRACT          : integer := 15;
-            COEFFICIENT_WIDTH : integer := 16;
-            COEFFICIENT_FRACT : integer := 16;
-			INTERNAL_WIDTH    : integer := 40;
-			INTERNAL_FRACT    : integer := 31;
+            IN_FRACT          : integer := 14;
+            COEFFICIENT_WIDTH : integer := 20;
+            COEFFICIENT_FRACT : integer := 18;
+			INTERNAL_WIDTH    : integer := 17;
+			INTERNAL_FRACT    : integer := 14;
 			OUT_WIDTH         : integer := 16;
-			OUT_FRACT         : integer := 15);
+			OUT_FRACT         : integer := 13);
    port(clk    : in  std_logic;
         clk_en : in  std_logic;
+		reset  : in  std_logic;
         x      : in  std_logic_vector(IN_WIDTH-1          downto 0);
         B0     : in  std_logic_vector(COEFFICIENT_WIDTH-1 downto 0);
         B1     : in  std_logic_vector(COEFFICIENT_WIDTH-1 downto 0);
@@ -65,9 +90,9 @@ architecture behaviour of Generic_IIR_SO is
   type array_input       is array(0 to N-1) of std_logic_vector(IN_WIDTH-1          downto 0);
   type array_coeffecient is array(0 to N-1) of std_logic_vector(COEFFICIENT_WIDTH-1 downto 0);
   type array_internal    is array(0 to N-1) of signed          (INTERNAL_WIDTH-1    downto 0);
-  type array_product     is array(0 to N-1) of signed          (INTERNAL_WIDTH + COEFFICIENT_WIDTH -1  downto 0);
+  type array_product     is array(0 to N-1) of signed          (PRODUCT_WIDTH-1     downto 0);
   
-  -- Koefficients
+  -- Coefficients
   signal coefficients_b : array_coeffecient;
   signal coefficients_a : array_coeffecient;
   
@@ -80,8 +105,10 @@ architecture behaviour of Generic_IIR_SO is
   signal my_mults_out : array_internal := (others => (others => '0'));
   signal my_sum_in    : array_internal := (others => (others => '0'));
   signal my_sum_out   : array_internal := (others => (others => '0'));
+  
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+
 begin
 
   -- Assign coefficients
@@ -90,6 +117,7 @@ begin
   coefficients_b(2) <= B2;
   coefficients_a(1) <= A1;
   coefficients_a(2) <= A2;
+
 
   -- Shift the delay registers
   my_outputs(0) <= my_sum_out(0);
@@ -104,6 +132,11 @@ begin
         my_outputs(1 to N-1) <= my_outputs(0 to N-2);
       end if;
     end if;
+	-- Asynchronous reset
+	if(reset = '1') then
+	  my_inputs            <= (others => (others => '0'));
+	  my_outputs(1 to N-1) <= (others => (others => '0'));
+	end if;
   end process p_shift_inputs;
   
   
@@ -141,7 +174,7 @@ begin
   
   
   -- Output the result
-  y <= std_logic_vector(my_outputs(0)(INTERNAL_FRACT + OUT_INT-1 downto INTERNAL_FRACT - OUT_FRACT)); -- Temp
+  y <= std_logic_vector(my_outputs(0)(INTERNAL_FRACT + OUT_INT-1 downto INTERNAL_FRACT - OUT_FRACT));
 end behaviour;
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
