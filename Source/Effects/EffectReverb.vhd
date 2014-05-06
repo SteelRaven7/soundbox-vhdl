ce8f40fcf0462bef2fa80c0bb21ff5f2ac5d41ff
Johan Hassel <johan_sr7@hotmail.com>
2014-05-06 16:03:11 +0200

    Added Martin

--- Source/Effects/EffectReverb.vhd
+++ Source/Effects/EffectReverb.vhd
@@ -2,14 +2,17 @@ library ieee ;
 	use ieee.std_logic_1164.all;
 	use ieee.numeric_std.all;
 
+
 entity EffectReverb is
 	generic(IO_length : integer := 16;
 			c_length : integer := 16;
-			addr_length : integer := 12); --Maximum delay is 4095 samples for all stages.
+			addr_length : integer := 12); --Maximum delay is 0 to 4095 samples for all stages.
 	port(input : in std_logic_vector(IO_length-1 downto 0);
 			output : out std_logic_vector(IO_length-1 downto 0);
-			--gain : in std_logic_vector(c_length-1 downto 0);
-			-- SCLK : in std_logic;
+			delay1conf : configurableRegisterBus;
+			delay2conf : configurableRegisterBus;
+			delay3conf : configurableRegisterBus;
+			delay4conf : configurableRegisterBus;
 			CLK : in std_logic;
 			RESET : in std_logic);
 end EffectReverb;
@@ -18,22 +21,39 @@ architecture behav of EffectReverb is
 	
 	type signal_array is array(3 downto 0) of std_logic_vector(c_length-1 downto 0);
 	type gain_array is array(3 downto 0) of signed(IO_length+c_length-1 downto 0);
-	type sum_array is array(3 downto 0) of std_logic_vector(IO_length+c_length-1 downto 0);
-	
+	type gain_array2 is array(3 downto 0) of signed(IO_length+c_length+3 downto 0);
+	type pad_array is array(3 downto 0) of std_logic_vector(2+c_length-1 downto 0);
 	
 	constant coeff_pos : std_logic_vector(c_length-1 downto 0) := x"7F5B";
 	constant coeff_neg : std_logic_vector(c_length-1 downto 0) := x"80A5";
-	constant wet_coeff : std_logic_vector(c_length-1 downto 0) := x"7FFF";
+
+	constant wet_coeff : std_logic_vector(c_length-1 downto 0) := x"2000";
+	constant dry_coeff : std_logic_vector(c_length-1 downto 0) := x"4000";
+	
+	signal addrMax1 : integer range 0 to 4095;
+	signal delay1par : std_logic_vector(c_length-1 down);
+	signal addrMax2 : integer range 0 to 4095;
+	signal delay2par : std_logic_vector(c_length-1 down);
+	signal addrMax3 : integer range 0 to 4095;
+	signal delay3par : std_logic_vector(c_length-1 down);
+	signal addrMax4 : integer range 0 to 4095;
+	signal delay4par : std_logic_vector(c_length-1 down);
 
 	signal gain_outputs : gain_array;
 	signal scalar : gain_array;
 	signal matrix_outputs : signal_array;
 	
-	signal dry_gain : std_logic_vector(IO_length+c_length-1 downto 0);
+	signal dry_gain : std_logic_vector(c_length-1 downto 0);
+	
+	signal wet_padded_sig : pad_array;
+	signal wet_padded_coe : std_logic_vector(2+IO_length-1 downto 0);
+	signal dry_padded_sig : std_logic_vector(2+IO_length-1 downto 0);
+	signal dry_padded_coe : std_logic_vector(2+IO_length-1 downto 0);
 	
 	signal delay_outputs : signal_array;
-	signal wet_gain : gain_array;
-	signal wet_sum : sum_array;
+	signal wet_gain : gain_array2;
+	signal wet_sum : signal_array;
+	signal wet_gain2 : signal_array;
 	signal wet_2_out : std_logic_vector(2*IO_length-1 downto 0);
 	signal input_sum : signal_array;
 
@@ -105,10 +125,10 @@ architecture behav of EffectReverb is
 
 	constant waitTime : natural := 4;
 	
-	constant addrMax1 : natural := 313;
-	constant addrMax2 : natural := 449;
-	constant addrMax3 : natural := 677;
-	constant addrMax4 : natural := 829;
+	--constant addrMax1 : natural := 313;
+	--constant addrMax2 : natural := 449;
+	--constant addrMax3 : natural := 677;
+	--constant addrMax4 : natural := 829;
 
 	type reg_type is record
 		state : state_type;
@@ -126,8 +146,105 @@ architecture behav of EffectReverb is
 
 	signal r, rin : reg_type;
 	
-	begin
+	function addsat (a,b : std_logic_vector) return std_logic_vector is
+		constant MAX : std_logic_vector(a'length-1 downto 0) := '0' & (a'length-2 downto 0 => '1');
+		constant MIN : std_logic_vector(a'length-1 downto 0) := '1' & (a'length-2 downto 0 => '0');
+		variable sum : std_logic_vector(a'length-1 downto 0);
+		variable overflow : std_logic;
+		variable s_a : std_logic;
+		variable s_b : std_logic;
+		variable s_s : std_logic;
+		begin
+			sum := std_logic_vector(signed(a) + signed(b));
+
+			s_a := a(a'length-1);
+			s_b := b(b'length-1);
+			s_s := sum(a'length-1);
+
+	-- Signs of a and b are the same, but not equal to sign of s means overflow.
+			overflow := ((s_a and s_b) and not(s_s)) or ((not(s_a) and not(s_b)) and s_s);
+			
+			if overflow = '0' then
+				return sum;
+			elsif s_a = '0' then
+				return MAX;
+			elsif s_a = '1' then
+				return MIN;
+			else
+				return (a'length-1 downto 0 => '0');
+			end if;
+			
+	end addsat;
 	
+	begin
+		
+			delay1Reg: entity work.ConfigRegister
+	generic map(
+		wordLength => 12;
+		address => std_logic_vector(to_unsigned(7,16));
+	)
+	port map(
+		input => delay1conf;
+		output => delay1par;
+
+		reset => reset;
+	);
+	
+	delay2Reg: entity work.ConfigRegister
+	generic map(
+		wordLength => 12;
+		address => std_logic_vector(to_unsigned(8,16));
+	)
+	port map(
+		input => delay2conf;
+		output => delay2par;
+
+		reset => reset;
+	);
+	
+	delay3Reg: entity work.ConfigRegister
+	generic map(
+		wordLength => 12;
+		address => std_logic_vector(to_unsigned(9,16));
+	)
+	port map(
+		input => delay3conf;
+		output => delay3par;
+
+		reset => reset;
+	);
+	
+	delay4Reg: entity work.ConfigRegister
+	generic map(
+		wordLength => 12;
+		address => std_logic_vector(to_unsigned(10,16));
+	)
+	port map(
+		input => delay4conf;
+		output => delay4par;
+
+		reset => reset;
+	);
+	
+		addrMax1 <= to_integer(unsigned(delay1par));
+		addrMax2 <= to_integer(unsigned(delay2par));
+		addrMax3 <= to_integer(unsigned(delay3par));
+		addrMax4 <= to_integer(unsigned(delay4par));
+		
+		wet_padded_sig(0) <= "00" & delay_outputs(0) when delay_outputs(0)(IO_length-1) = '0' else
+							"11" & delay_outputs(0);
+		wet_padded_sig(1) <= "00" & delay_outputs(1) when delay_outputs(1)(IO_length-1) = '0' else
+							"11" & delay_outputs(1);
+		wet_padded_sig(2) <= "00" & delay_outputs(2) when delay_outputs(2)(IO_length-1) = '0' else
+							"11" & delay_outputs(2);
+		wet_padded_sig(3) <= "00" & delay_outputs(3) when delay_outputs(3)(IO_length-1) = '0' else
+							"11" & delay_outputs(3);
+							
+		wet_padded_coe <= wet_coeff & "00";
+		dry_padded_coe <= dry_coeff & "00";
+		dry_padded_sig <= "00" & input when input(IO_length-1) = '0' else
+						"11" & input;
+		
 		--Each delayed signal times it's feedback coefficient, vector*vector transpose
 		gain_outputs(0) <= signed(delay_outputs(0))*signed(coeff_pos);
 		gain_outputs(1) <= signed(delay_outputs(1))*signed(coeff_pos);
@@ -154,113 +271,30 @@ architecture behav of EffectReverb is
 		matrix_outputs(3) <= std_logic_vector(scalar(3)(IO_length+c_length-1 downto IO_length));
 		
 		--Output gains of the delay net
-		wet_gain(0) <= signed(delay_outputs(0))*signed(wet_coeff);
-		wet_gain(1) <= signed(delay_outputs(1))*signed(wet_coeff);
-		wet_gain(2) <= signed(delay_outputs(2))*signed(wet_coeff);
-		wet_gain(3) <= signed(delay_outputs(3))*signed(wet_coeff);
-		dry_gain <= input & x"0000" when input(15) = '0' else
-					input & x"FFFF";
-
+		wet_gain(0) <= signed(wet_padded_sig(0))*signed(wet_padded_coe);
+		wet_gain(1) <= signed(wet_padded_sig(1))*signed(wet_padded_coe);
+		wet_gain(2) <= signed(wet_padded_sig(2))*signed(wet_padded_coe);
+		wet_gain(3) <= signed(wet_padded_sig(3))*signed(wet_padded_coe);
 		
+		wet_gain2(0) <= std_logic_vector(wet_gain(0)(2*IO_length-1 downto IO_length));
+		wet_gain2(1) <= std_logic_vector(wet_gain(1)(2*IO_length-1 downto IO_length));
+		wet_gain2(2) <= std_logic_vector(wet_gain(2)(2*IO_length-1 downto IO_length));
+		wet_gain2(3) <= std_logic_vector(wet_gain(3)(2*IO_length-1 downto IO_length));
 		
-		--Summation of the wet signals and the dry input
-	   -- a:for i in 1 to 3 generate
-		accuma : entity work.AdderSat
-		generic map (
-			wordLength => 2*IO_length
-		)
-		port map (
-			a => std_logic_vector(wet_gain(0)),
-			b => wet_sum(0),
-
-			s => wet_sum(1)
-		);
-
-		accumb : entity work.AdderSat
-		generic map (
-			wordLength => 2*IO_length
-		)
-		port map (
-			a => std_logic_vector(wet_gain(1)),
-			b => wet_sum(1),
-
-			s => wet_sum(2)
-		);
-
-		accumc : entity work.AdderSat
-		generic map (
-			wordLength => 2*IO_length
-		)
-		port map (
-			a => std_logic_vector(wet_gain(2)),
-			b => wet_sum(2),
-
-			s => wet_sum(3)
-		);
-
+		dry_gain <= input;
+					
+		wet_sum(0) <= addsat(wet_gain2(0),dry_gain);
+		wet_sum(1) <= addsat(wet_gain2(1),wet_sum(0));
+		wet_sum(2) <= addsat(wet_gain2(2),wet_sum(1));
+		wet_sum(3) <= addsat(wet_gain2(3),wet_sum(2));
 		
-		-- end generate;
+		output <= wet_sum(3);
 		
-	   -- b:for i in 0 to 3 generate
-		accum2 : entity work.AdderSat
-		generic map (
-			wordLength => IO_length
-		)
-		port map (
-			a => input,
-			b => matrix_outputs(0),
-
-			s => input_sum(0)
-		);
-
-		accum3 : entity work.AdderSat
-		generic map (
-			wordLength => IO_length
-		)
-		port map (
-			a => input,
-			b => matrix_outputs(1),
-
-			s => input_sum(1)
-		);
-
-		accum4 : entity work.AdderSat
-		generic map (
-			wordLength => IO_length
-		)
-		port map (
-			a => input,
-			b => matrix_outputs(2),
-
-			s => input_sum(2)
-		);
-
-		accum5 : entity work.AdderSat
-		generic map (
-			wordLength => IO_length
-		)
-		port map (
-			a => input,
-			b => matrix_outputs(3),
-
-			s => input_sum(3)
-		);
-		-- end generate;
+		input_sum(0) <= addsat(input,matrix_outputs(0));
+		input_sum(1) <= addsat(input,matrix_outputs(1));
+		input_sum(2) <= addsat(input,matrix_outputs(2));
+		input_sum(3) <= addsat(input,matrix_outputs(3));
 		
-		accum1 : entity work.AdderSat
-		generic map (
-			wordLength => 2*IO_length
-		)
-		port map (
-			a => dry_gain,
-			b => std_logic_vector(wet_gain(3)),
-
-			s => wet_sum(0)
-		);
-		
-		wet_2_out <= std_logic_vector(wet_sum(3));
-		output <= wet_2_out(2*IO_length-1 downto IO_length);
-
 		memory_1:blk_mem_gen_delay1
 		port map (
 			dina => delay1.dataIn,
